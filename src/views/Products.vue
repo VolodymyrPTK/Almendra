c24f5a290afb899bb48d82368a84ec480cd58026
Volodymyr Pityk <vladimirpityk@gmail.com>
2022-06-30 00:22:37 +0300

    fixed id on console log

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -15,13 +15,14 @@
       <button class="productbutton" @click="saveData">Зберегти</button>
     </div>
     <div class="productlist">
-      <table>
+      <table class="fixed_headers">
         <thead>
           <tr>
             <th>Назва</th>
             <th>Бренд</th>
             <th>Категорія</th>
             <th>Ціна</th>
+            <th>Зміни</th>
           </tr>
         </thead>
         <tbody>
@@ -29,8 +30,10 @@
             <td>{{ product.name }}</td>
             <td>{{ product.brand }}</td>
             <td>{{ product.category }}</td>
-            <td>{{ product.price }}</td>
-            <button>delete</button>
+            <td>₴ {{ product.price }}</td>
+            <td class="tablebtns"><button class="editButton">≡</button>
+              <button class="deleteButton" @click="deleteProduct(product)">X</button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -64,15 +67,12 @@ export default {
   methods: {
     saveData() {
       try {
-        const docRef = addDoc(dataBase, this.product);
-        console.log("Document written with ID: ", docRef.id);
-        this.reset();
+        addDoc(dataBase, this.product).then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+        });
       } catch (e) {
         console.error("Error adding document: ", e);
       }
-    },
-    reset() {
-      Object.assign(this.$data, this.$options.data.apply(this));
     }
   },
   created() {
@@ -85,7 +85,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .products {
   display: flex;
   width: 100%;
@@ -169,5 +169,100 @@ textarea {
 .productbutton:active {
   box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.3), inset 0px 3px 5px rgba(0, 0, 0, 0.3);
   transition: 0.1s;
+}
+
+.editButton {
+  background-color: rgb(255, 229, 143);
+  border: none;
+  border-radius: 50px;
+}
+
+.editButton:hover {
+  background-color: rgb(255, 211, 68);
+}
+
+.editButton:active {
+  background-color: rgb(253, 202, 36);
+}
+
+.deleteButton {
+  background-color: rgb(255, 130, 72);
+  border: none;
+  border-radius: 50px;
+}
+
+.deleteButton:hover {
+  background-color: rgb(252, 74, 74);
+}
+
+.deleteButton:active {
+  background-color: rgb(255, 42, 42);
+}
+
+.fixed_headers {
+  height: 90%;
+  width: 90%;
+  table-layout: fixed;
+  border-collapse: collapse;
+
+  th {
+    text-decoration: underline;
+  }
+
+  th,
+  td {
+    padding: 5px;
+    text-align: left;
+  }
+
+  td:nth-child(1),
+  th:nth-child(1) {
+    width: 40%;
+  }
+
+  td:nth-child(2),
+  th:nth-child(2) {
+    width: 25%;
+  }
+
+  td:nth-child(3),
+  th:nth-child(3) {
+    width: 25%;
+  }
+
+  td:nth-child(4),
+  th:nth-child(4) {
+    width: 10%;
+  }
+
+  td:nth-child(5),
+  th:nth-child(5) {
+    width: 10%;
+  }
+
+  thead {
+    background-color: rgb(177, 177, 177);
+    color: #FDFDFD;
+
+    tr {
+      display: block;
+      position: relative;
+    }
+  }
+
+  tbody {
+    display: block;
+    overflow: auto;
+    width: 100%;
+    height: 90%;
+
+    tr:nth-child(even) {
+      background-color: #DDD;
+    }
+  }
+}
+
+.tablebtns {
+  display: flex;
 }
 </style>
